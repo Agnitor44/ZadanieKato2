d76fdb46410570dba296fee6ce289ccb903c2665
Piotr Swis <zmorekcraftx@gmail.com>
2023-04-05 20:31:55 +0200

    fix url

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-const urlFirst = `https://samples.openweathermap.org/data/2.5/forecast?q=`;
-const urlSecond = `,us&appid=`;
+const urlFirst = `https://api.openweathermap.org/data/2.5/forecast?q=`;
+const urlSecond = `,&appid=`;
 
 export const fetchData = (location, setState, setError, setIsLoading) => {
   setIsLoading(true);
   axios
     .get(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${location},&appid=${process.env.NEXT_PUBLIC_APP_ID}`,
+      `${urlFirst}${location}${urlSecond}${process.env.NEXT_PUBLIC_APP_ID}`,
       {}
     )
     .then((res) => {
